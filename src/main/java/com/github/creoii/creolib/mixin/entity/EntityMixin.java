0ee63830ff9bf815a50656c31c222cfdffe2b400
creoii <33011337+jack-zisa@users.noreply.github.com>
2023-02-04 12:45:08 -0500

    Walks on Water/Lava Entity Type Tags
    Rename immune/* tags to damage_immune/*
    Projectiles Ignore Block Tag
    Change Tripwire Ignores to Avoids Traps

--- src/main/java/com/github/creoii/creolib/mixin/entity/EntityMixin.java
+++ src/main/java/com/github/creoii/creolib/mixin/entity/EntityMixin.java
@@ -4,13 +4,18 @@ import com.github.creoii.creolib.tag.CBlockTags;
 import com.github.creoii.creolib.tag.CEntityTypeTags;
 import com.google.common.collect.ImmutableMap;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.FluidBlock;
+import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.MovementType;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.registry.tag.EntityTypeTags;
+import net.minecraft.registry.tag.FluidTags;
 import net.minecraft.registry.tag.TagKey;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -23,7 +28,16 @@ public abstract class EntityMixin {
     @Shadow public boolean noClip;
     @Shadow public abstract BlockState getBlockStateAtPos();
     @Shadow public abstract EntityType<?> getType();
+    @Shadow public abstract World getWorld();
+    @Shadow protected abstract void checkBlockCollision();
+    @Shadow public abstract void onLanding();
+    @Shadow public abstract boolean isInLava();
+    @Shadow public abstract boolean isTouchingWater();
+    @Shadow public abstract BlockPos getBlockPos();
+    @Shadow public abstract void setVelocity(Vec3d velocity);
+    @Shadow public abstract Vec3d getVelocity();
 
+    @Shadow protected boolean onGround;
     private static final ImmutableMap<DamageSource, TagKey<EntityType<?>>> DAMAGE_IMMUNITIES = new ImmutableMap.Builder<DamageSource, TagKey<EntityType<?>>>()
             .put(DamageSource.CACTUS, CEntityTypeTags.CACTUS_IMMUNE)
             .put(DamageSource.SWEET_BERRY_BUSH, CEntityTypeTags.SWEET_BERRY_BUSH_IMMUNE)
@@ -62,6 +76,37 @@ public abstract class EntityMixin {
     private void creo_lib_damageImmunities(DamageSource damageSource, CallbackInfoReturnable<Boolean> cir) {
         if (DAMAGE_IMMUNITIES.containsKey(damageSource)) {
             cir.setReturnValue(getType().isIn(DAMAGE_IMMUNITIES.get(damageSource)));
+        }
+    }
+
+    @Inject(method = "tick", at = @At("TAIL"))
+    private void creo_lib_walkOnFluids(CallbackInfo ci) {
+        if (getType().isIn(CEntityTypeTags.WALKS_ON_WATER) || getType().isIn(CEntityTypeTags.WALKS_ON_LAVA)) {
+            ShapeContext shapeContext = ShapeContext.of((Entity) (Object) this);
+            if (isInLava() || isTouchingWater()) {
+                if (!shapeContext.isAbove(FluidBlock.COLLISION_SHAPE, getBlockPos(), true) || getWorld().getFluidState(getBlockPos().up()).isIn(isTouchingWater() ? FluidTags.WATER : FluidTags.LAVA)) {
+                    setVelocity(getVelocity().multiply(.5d).add(0d, .05d, 0d));
+                } else onGround = true;
+            }
+            checkBlockCollision();
+        }
+    }
+
+    @Inject(method = "fall", at = @At("TAIL"), cancellable = true)
+    private void creo_lib_landOnFluids(CallbackInfo ci) {
+        if (getType().isIn(CEntityTypeTags.WALKS_ON_WATER) || getType().isIn(CEntityTypeTags.WALKS_ON_LAVA)) {
+            checkBlockCollision();
+            if ((isInLava() && getType().isIn(CEntityTypeTags.WALKS_ON_LAVA)) || (isTouchingWater() && getType().isIn(CEntityTypeTags.WALKS_ON_WATER))) {
+                onLanding();
+                ci.cancel();
+            }
+        }
+    }
+
+    @Inject(method = "canAvoidTraps", at = @At("HEAD"), cancellable = true)
+    private void creo_lib_canAvoidTraps(CallbackInfoReturnable<Boolean> cir) {
+        if (getType().isIn(CEntityTypeTags.AVOIDS_TRAPS)) {
+            cir.setReturnValue(true);
         }
     }
 }
--- src/main/java/com/github/creoii/creolib/tag/CEntityTypeTags.java
+++ src/main/java/com/github/creoii/creolib/tag/CEntityTypeTags.java
@@ -8,24 +8,24 @@ import net.minecraft.util.Identifier;
 
 public class CEntityTypeTags {
     public static final TagKey<EntityType<?>> NO_CLIPPING_ENTITIES = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "no_clipping_entities"));
-    public static final TagKey<EntityType<?>> CACTUS_IMMUNE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "immune/cactus"));
-    public static final TagKey<EntityType<?>> SWEET_BERRY_BUSH_IMMUNE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "immune/sweet_berry_bush"));
-    public static final TagKey<EntityType<?>> DRIPSTONE_IMMUNE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "immune/dripstone"));
-    public static final TagKey<EntityType<?>> LIGHTNING_IMMUNE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "immune/lightning"));
-    public static final TagKey<EntityType<?>> DROWNING_IMMUNE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "immune/drowning"));
-    public static final TagKey<EntityType<?>> MAGIC_IMMUNE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "immune/magic"));
-    public static final TagKey<EntityType<?>> FIRE_IMMUNE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "immune/fire"));
-    public static final TagKey<EntityType<?>> WITHER_IMMUNE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "immune/wither"));
+    public static final TagKey<EntityType<?>> CACTUS_IMMUNE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "damage_immune/cactus"));
+    public static final TagKey<EntityType<?>> SWEET_BERRY_BUSH_IMMUNE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "damage_immune/sweet_berry_bush"));
+    public static final TagKey<EntityType<?>> DRIPSTONE_IMMUNE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "damage_immune/dripstone"));
+    public static final TagKey<EntityType<?>> LIGHTNING_IMMUNE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "damage_immune/lightning"));
+    public static final TagKey<EntityType<?>> DROWNING_IMMUNE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "damage_immune/drowning"));
+    public static final TagKey<EntityType<?>> MAGIC_IMMUNE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "damage_immune/magic"));
+    public static final TagKey<EntityType<?>> FIRE_IMMUNE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "damage_immune/fire"));
+    public static final TagKey<EntityType<?>> WITHER_IMMUNE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "damage_immune/wither"));
     public static final TagKey<EntityType<?>> ZOGLIN_IGNORES = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "zoglin_ignores"));
-    public static final TagKey<EntityType<?>> TRIPWIRE_IGNORES = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "tripwire_ignores"));
+    public static final TagKey<EntityType<?>> AVOIDS_TRAPS = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "avoids_traps"));
     public static final TagKey<EntityType<?>> PROJECTILES_IGNORE = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "projectiles_ignore"));
+    public static final TagKey<EntityType<?>> WALKS_ON_WATER = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "walks_on_water"));
+    public static final TagKey<EntityType<?>> WALKS_ON_LAVA = TagKey.of(RegistryKeys.ENTITY_TYPE, new Identifier(CreoLib.COMMON, "walks_on_lava"));
 
     /**
      * TODO:
      *
      * Milkable
-     * Projectiles Ignore
-     * Walks On Fluids
      * Goat Unrammable
      * Wither Resistant
      * Dragon Resistant
