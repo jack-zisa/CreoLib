93b26ee87e5ac0de8a56a6f5f15e878b6f3d70f6
creoii <33011337+jack-zisa@users.noreply.github.com>
2023-02-06 18:13:02 -0500

    Biome Fog affects all entities

--- src/main/java/com/github/creoii/creolib/mixin/client/BackgroundRendererMixin.java
+++ src/main/java/com/github/creoii/creolib/mixin/client/BackgroundRendererMixin.java
@@ -4,7 +4,6 @@ import com.github.creoii.creolib.util.BiomeFogModifier;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
 import net.minecraft.client.render.CameraSubmersionType;
@@ -23,17 +22,15 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 public class BackgroundRendererMixin {
     @Inject(method = "applyFog", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;setShaderFogStart(F)V"), locals = LocalCapture.CAPTURE_FAILSOFT, cancellable = true)
     private static void creo_lib_applyBiomeFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo ci, CameraSubmersionType cameraSubmersionType, Entity entity, BackgroundRenderer.FogData fogData) {
-        if (entity instanceof ClientPlayerEntity clientPlayerEntity) {
-            RegistryEntry<Biome> registryEntry = clientPlayerEntity.world.getBiome(clientPlayerEntity.getBlockPos());
-            if (registryEntry.getKey().isPresent()) {
-                RegistryKey<Biome> biomeKey = registryEntry.getKey().get();
-                if (BiomeFogModifier.BIOME_FOG_MODIFIERS.containsKey(biomeKey)) {
-                    BiomeFogModifier modifier = BiomeFogModifier.BIOME_FOG_MODIFIERS.get(biomeKey);
-                    RenderSystem.setShaderFogStart(viewDistance * modifier.fogStart());
-                    RenderSystem.setShaderFogEnd(Math.min(viewDistance, 192f) * modifier.fogEnd());
-                    RenderSystem.setShaderFogShape(modifier.fogShape());
-                    ci.cancel();
-                }
+        RegistryEntry<Biome> registryEntry = entity.world.getBiome(entity.getBlockPos());
+        if (registryEntry.getKey().isPresent()) {
+            RegistryKey<Biome> biomeKey = registryEntry.getKey().get();
+            if (BiomeFogModifier.BIOME_FOG_MODIFIERS.containsKey(biomeKey)) {
+                BiomeFogModifier modifier = BiomeFogModifier.BIOME_FOG_MODIFIERS.get(biomeKey);
+                RenderSystem.setShaderFogStart(viewDistance * modifier.fogStart());
+                RenderSystem.setShaderFogEnd(Math.min(viewDistance, 192f) * modifier.fogEnd());
+                RenderSystem.setShaderFogShape(modifier.fogShape());
+                ci.cancel();
             }
         }
     }
